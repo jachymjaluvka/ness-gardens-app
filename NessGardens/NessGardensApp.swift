b392295b579f7e073b2a064bb6698675769b58c3
Jachym Jaluvka <jachym.jaluvka@statsperform.com>
2023-02-13 18:12:34 +0000

    Initial Commit

---
+++ NessGardens/NessGardensApp.swift
@@ -0,0 +1,17 @@
+//
+//  NessGardensApp.swift
+//  NessGardens
+//
+//  Created by Jachym Jaluvka on 13.02.2023.
+//
+
+import SwiftUI
+
+@main
+struct NessGardensApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
